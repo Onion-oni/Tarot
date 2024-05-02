02a6c372873ef84c52751b716fca5995768b7e7e
Andrew Holmes <167374972+Onion-oni@users.noreply.github.com>
2024-05-01 17:07:13 -0700

    Update Tarot.py
    Added draw method to TarotDeck class and made small naming changes

--- Tarot.py
+++ Tarot.py
@@ -2,7 +2,7 @@ pips = ['0', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'Page'
 suits = ['Pentacles', 'Swords', 'Wands', 'Cups']
 arcana = ['0 - Fool', 'I - Magician', 'II - High Priestess', 'III - Empress', 'IV - Emperor', 'V - Hierophant', 'VI - Lovers', 'VII - Chariot', 'VIII - Strength', 'IX - Hermit', 'X - Wheel of Fortune', 'XI - Justice', 'XII - The Hanged Man', 'XIII - Death', 'XIV - Temperance', 'XV - Devil', 'XVI - The Tower', 'XVII - The Star', 'XVIII - The Moon', 'XIX - The Sun', 'XX - Judgement', 'XXI - The World']
 
-class Minor_Tarot_Card():
+class MinorTarotCard():
 
     def __init__ (self, pips,suits):
         self.suits = suits
@@ -11,7 +11,7 @@ class Minor_Tarot_Card():
     def __str__(self):
         return self.pips + " of " + self.suits
         
-class Major_Tarot_Card:
+class MajorTarotCard:
 
     def __init__ (self, arcana):
         self.arcana = arcana
@@ -19,7 +19,7 @@ class Major_Tarot_Card:
     def __str__(self):
         return self.arcana
 
-class Tarot_Deck:
+class TarotDeck:
 
     def __init__(self):
 
@@ -28,7 +28,18 @@ class Tarot_Deck:
 
         for pip in pips:
             for suit in suits:
-                minor_tarot_card = Minor_Tarot_Card(pip,suit)
+                minor_tarot_card = MinorTarotCard(pip,suit)
                 self.minor_arcana.append(minor_tarot_card)
-
         self.all_cards = self.minor_arcana + self.major_arcana
+        
+    def draw(self,x):
+        #x is the number of cards for a spread
+        counter = 0
+        spread = []
+        self.all_cards = self.minor_arcana + self.major_arcana
+        while counter != x:
+            #self.all_cards.random()
+            spread.append(self.all_cards.pop())
+            counter = counter + 1
+        else:
+            return spread
